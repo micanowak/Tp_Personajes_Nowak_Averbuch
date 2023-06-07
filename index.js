e0cdfaada8953baf8ba2ab00eb5e512d1e73d5d1
MICAELA NOWAK <47297838@ort>
2023-06-07 08:22:06 -0300

    index terminado aunque no lo verificamos en postman todavía

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ app.post('/Personaje', async (req, res) => {
 })
 
 app.get('/Personaje', async (req, res) => {
-    const pizza = await PersonajeServices.getNotAllAtributos(req.params.id)
+    const Personaje = await PersonajeServices.getNotAllAtributos()
     res.status(200).send(Personaje)
 })
 
@@ -66,34 +66,71 @@ app.get('/Personaje/:peso', async (req, res) => {
 
 })
 
-
-
-
-/*app.get('/Personaje', async (req, res) => {
-    const pizza = await PersonajeServices.getAll(req.params.id)
-    res.status(200).send(pizza)
-})
-
-app.get('/Personaje/:id', async (req, res) => {
-    const Personaje = await PersonajeServices.getById(req.params.id)
+app.get('/Personaje/:idPelicula', async (req, res) => {
+    const Personaje = await PersonajeServices.getByPeliserie(req.params.idPelicula)
     res.status(200).send(Personaje)
 
 })
 
-app.get('/Pizza', async (req, res) => {
-    const pizza = await PersonajeServices.getALL()
-    res.status(200).send(pizza)
+app.get('/Peliserie', async (req, res) => {
+    const Peliserie = await PersonajeServices.getNotAllAtributosPel()
+    res.status(200).send(Peliserie)
 
 })
 
-app.put('/Pizza/:id', async (req, res) => {
+app.get('/Peliserie', async (req, res) => {
+    const Peliserie = await PersonajeServices.getNotAllAtributosPel()
+    res.status(200).send(Peliserie)
+
+})
+
+app.get('/Peliserie', async (req, res) => {
+    const Peliserie = await PersonajeServices.getAllPeli()
+    res.status(200).send(Peliserie)
+
+})
+
+app.put('/Pelserie/:id', async (req, res) => {
     try {
-        await PersonajeServices.update(req.params.id, req.body);
-        res.status(200).json({ message: 'Pizza Actualizada'});
+        await PersonajeServices.updatePeli(req.params.id, req.body);
+        res.status(200).json({ message: 'Pelicula Actualizado'});
     } catch(error) {
         console.log(error);
         res.status(500).json({ error: 'Fallo el update' });
 
     }
 })
-*/
+
+app.delete('/Peliserie/:id', async (req, res) => {
+    try {
+        await PersonajeServices.deleteByIdPeli(req.params.id);
+        res.status(200).json({ message: 'Peliserie Eliminado'});
+    } catch(error) {
+        console.log(error);
+        res.status(500).json({ error: 'Fallo el delete' });
+
+    }
+})
+
+app.post('/Peliserie', async (req, res) => {
+    console.log("en post, req:", req)
+    try {
+        await PersonajeServices.insert(req.body)
+        res.status(200).json({ message: 'Peliserie creada' });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Fallo el insert' });
+    }
+})
+
+app.get('/Peliserie/:titulo', async (req, res) => {
+    const Peliserie = await PersonajeServices.getByTitulo(req.params.titulo)
+    res.status(200).send(Peliserie)
+
+})
+
+app.get('/Peliserie/:order', async (req, res) => {
+    const Peliserie = await PersonajeServices.getAllOrdered(req.params.order)
+    res.status(200).send(Peliserie)
+
+})
